3606f7962046211fd171af6cb9b34a779ef0468a
ranael-garem <ranaelgarem781994@gmail.com>
2015-07-13 11:54:57 +0200

    Added Actor and Experience model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Actor(models.Model):
+    user = models.OneToOneField(User)
+
+
+class Experience(models.Model):
+    experience = models.CharField(max_length=200)
+    actor = models.ForeignKey(Actor, related_name='experiences')
