bba05f67fa18b7760d60109778c5561a08978ff0
ranael-garem <ranaelgarem781994@gmail.com>
2015-07-13 11:58:07 +0200

    Added ActorViewSet and ExperienceViewSet

--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from main.models import Actor, Experience
+from rest_framework import viewsets
+from main.serializers import ActorSeriealizer, ExperienceSerializer
 
-# Create your views here.
+
+class ActorViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSeriealizer
+
+
+class ExperienceViewSet(viewsets.ModelViewSet):
+    queryset = Experience.objects.all()
+    serializer_class = ExperienceSerializer
