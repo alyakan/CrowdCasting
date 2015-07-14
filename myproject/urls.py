ad3d9177e66936291dee706206cb136ec97410a7
ranael-garem <ranaelgarem781994@gmail.com>
2015-07-14 12:55:01 +0200

    modifed myproject/urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,7 +15,26 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+from main.views import ActorViewSet, ExperienceViewSet
+
+router = DefaultRouter()
+router.register(r'actor', ActorViewSet)
+router.register(r'experience', ExperienceViewSet)
+
+experience_detail = ExperienceViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^experience/(?P<pk>[0-9]+)/$',
+        experience_detail, name='experience_detail'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api-auth/',
+        include('rest_framework.urls', namespace='rest_framework')),
 ]
