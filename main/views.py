db6b2451de47e55439757fab3f2d1ed0a2daa839
ranael-garem <ranaelgarem781994@gmail.com>
2015-07-14 12:54:33 +0200

    added permission classes to views and added perform_create method to ExperienceViewSet

--- main/views.py
+++ main/views.py
@@ -1,13 +1,19 @@
 from main.models import Actor, Experience
-from rest_framework import viewsets
-from main.serializers import ActorSeriealizer, ExperienceSerializer
+from rest_framework import viewsets, permissions
+from main.serializers import ActorSerializer, ExperienceSerializer
+from main.permissions import IsOwnerOrReadOnly
 
 
 class ActorViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Actor.objects.all()
-    serializer_class = ActorSeriealizer
+    serializer_class = ActorSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class ExperienceViewSet(viewsets.ModelViewSet):
     queryset = Experience.objects.all()
     serializer_class = ExperienceSerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwnerOrReadOnly)
+
+    def perform_create(self, serializer):
+        serializer.save(actor=Actor.objects.get(id=self.request.user.id))
