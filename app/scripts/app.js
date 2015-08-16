cf6141b95026665e76a60cf9df127b242f566c57
veegas <mohamed.yasser.abdel.razek@gmail.com>
2015-08-16 11:48:58 +0200

    Yo angular generator but removed compass and used libsass instead to compile sass

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name crowdCastingApp
+ * @description
+ * # crowdCastingApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('crowdCastingApp', [
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
