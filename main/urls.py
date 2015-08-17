b85193d33d5d4b60fd19e6d1b6deb9276e1ad613
Aly Yakan <alyyakan@hotmail.com>
2015-08-17 10:44:18 +0300

    Removed redundant views and urls

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url, include
 from main.views import (
-    ActorViewSet, ExperienceViewSet, ContactInfoViewSet,
-    RequestContactInfoViewSet,
-    TagViewSet, EducationViewSet)
+    ActorViewSet,
+    RequestContactInfoViewSet)
 from main import views
 from rest_framework.routers import DefaultRouter
 
@@ -20,40 +19,11 @@ actor_detail = ActorViewSet.as_view({
     'delete': 'destroy'
 })
 
-
-contactinfo_detail = ContactInfoViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-
-})
-
-experience_detail = ExperienceViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-
 request_contactinfo = RequestContactInfoViewSet.as_view({
     'get': 'retrieve',
     'delete': 'destroy'
 })
 
-tag_detail = TagViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-
-education_detail = EducationViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
 
 router = DefaultRouter()
 # router.register(r'head_shots', views.HeadShotsViewSet)
@@ -61,7 +31,7 @@ router = DefaultRouter()
 router.register(r'user', views.UserViewSet)
 router.register(r'actor', views.ActorViewSet, base_name='actor')
 # router.register(r'experience', views.ExperienceViewSet, base_name='experience')
-router.register(r'request_account', views.RequestAccountViewSet)
+# router.register(r'request_account', views.RequestAccountViewSet)
 # router.register(r'contactinfo', ContactInfoViewSet, base_name='contactinfo')
 router.register(r'request_contactinfo', RequestContactInfoViewSet)
 # router.register(r'profile_pictures', views.ProfilePictureViewSet)
--- main/views.py
+++ main/views.py
@@ -77,33 +77,6 @@ class DirectorViewSet(UserViewSet):
         login(self.request, user)
 
 
-class HeadShotsViewSet(viewsets.ModelViewSet):
-    queryset = HeadShots.objects.all()
-    serializer_class = HeadShotsSerializer
-    # permission_classes = (permissions.IsOwnerOrReadOnly,)
-
-    def perform_create(self, serializer):
-        serializer.save(user_id=self.request.user.id)
-
-
-class TrialViewSet(viewsets.ViewSet):
-    queryset = Trial.objects.all()
-
-    def list(self, request):
-        queryset = Trial.objects.all()
-        serializer = TrialSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def create(self, request):
-        listOfThings = request.DATA['names']
-
-        serializer = TrialSerializer(
-            data=listOfThings, many=True)
-        if serializer.is_valid():
-            serializer.save(user_id=self.request.user.id)
-            return Response(serializer.data)
-
-
 class ActorViewSet(viewsets.ModelViewSet):
     serializer_class = ActorSerializer
     permission_classes = (
@@ -122,67 +95,6 @@ class ActorViewSet(viewsets.ModelViewSet):
                 return Actor.objects.none()
         else:
             return Actor.objects.none()
-
-
-class ProfilePictureViewSet(viewsets.ModelViewSet):
-    queryset = ProfilePicture.objects.all()
-    serializer_class = ProfilePictureSerializer
-    permission_classes = (
-        myPermissions.IsPhotoUploaded,)
-
-    def perform_create(self, serializer):
-        actor = Actor.objects.get(user_id=self.request.user.id)
-        serializer.save(actor_id=actor.id)
-
-
-class ExperienceViewSet(viewsets.ModelViewSet):
-    serializer_class = ExperienceSerializer
-    permission_classes = (
-        permissions.IsAuthenticated, myPermissions.IsOwnerOrReadOnly)
-
-    def perform_create(self, serializer):
-        serializer.save(actor=Actor.objects.get(id=self.request.user.id))
-
-    def get_queryset(self):
-        if self.request.user and self.request.user.is_staff:
-            return Experience.objects.all()
-        elif self.request.user.is_authenticated():
-            return Experience.objects.filter(
-                actor_id=Actor.objects.filter(
-                    user_id=self.request.user.id))
-        else:
-            return Experience.objects.none()
-
-
-class EducationViewSet(viewsets.ModelViewSet):
-    serializer_class = EducationSerializer
-    permission_classes = (
-        permissions.IsAuthenticated, myPermissions.IsOwnerOrReadOnly)
-
-    def perform_create(self, serializer):
-        serializer.save(actor=Actor.objects.get(id=self.request.user.id))
-
-    def get_queryset(self):
-        if self.request.user and self.request.user.is_staff:
-            return Education.objects.all()
-        elif self.request.user.is_authenticated():
-            return Education.objects.filter(
-                actor_id=Actor.objects.filter(
-                    user_id=self.request.user.id))
-        else:
-            return Experience.objects.none()
-
-
-class RequestAccountViewSet(viewsets.ModelViewSet):
-    queryset = RequestAccountNotification.objects.all()
-    serializer_class = RequestAccountNotificationSerializer
-    permission_classes = (myPermissions.IsSuperUserOrTargetUser,)
-
-    def get_queryset(self):
-        if self.request.user and self.request.user.is_superuser:
-            return RequestAccountNotification.objects.all()
-        else:
-            return RequestAccountNotification.objects.none()
 
 
 class RequestContactInfoViewSet(viewsets.ModelViewSet):
@@ -227,40 +139,3 @@ class RequestContactInfoViewSet(viewsets.ModelViewSet):
                 return
         raise serializers.ValidationError(
                             'This username is invalid. Please try again.')
-
-
-class ContactInfoViewSet(viewsets.ModelViewSet):
-    """
-    This is a list of all phone numbers for the currently signed in user
-    author: Nourhan
-    """
-    serializer_class = ContactInfoSerializer
-    permission_classes = (permissions.IsAuthenticated,)
-    lookup_field = 'pk'
-
-    def get_queryset(self):
-        """
-        This view should return the contact info
-        for the currently authenticated user.
-        """
-        return ContactInfo.objects.filter(
-            actor=Actor.objects.get(user=self.request.user))
-
-    def perform_create(self, serializer):
-        queryset = ContactInfo.objects.filter(
-            actor=Actor.objects.get(user=self.request.user))
-
-        if queryset.count() > 0:
-            raise serializers.ValidationError(
-                'You are only allowed to post one phone number.')
-        else:
-            serializer.save(actor=Actor.objects.get(user=self.request.user))
-
-
-class TagViewSet(viewsets.ModelViewSet):
-    queryset = Tag.objects.all()
-    serializer_class = TagSerializer
-    permission_classes = (permissions.IsAuthenticated,)
-
-    def perform_create(self, serializer):
-        serializer.save(actor=Actor.objects.get(user=self.request.user))
