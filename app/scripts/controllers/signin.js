c787bb45d69db1c0c23ce62e119e37d376ec3775
mennanader <mennat.hafez@gmail.com>
2015-08-31 11:44:34 +0200

    retouches

--- app/scripts/controllers/signin.js
+++ app/scripts/controllers/signin.js
@@ -1,5 +1,15 @@
 'use strict'
 
 app.controller('SigninCtrl', function($http, $scope, $stateParams, $location) {
-	$scope.signup = "views/signin.html"
+	$scope.email;
+	$scope.password;
+	        $scope.login = function(){
+	        $http.post(HOSTED_URL + '/api-auth/login/', {
+            email: $scope.email,
+            password: $scope.password,
+            next: 'api/actor/'
+        }).then(function(response) {
+        	console.log('CSRF FAILED: ', response);
+        }, function(response) {})
+};
 })
